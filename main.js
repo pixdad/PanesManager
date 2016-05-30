2c43801dc29c1171dc148d99d49b60b327dca7a0
pixdad <david.oudart@gmail.com>
2016-05-30 17:51:57 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+define(function (require, exports, module) {
+
+	"use strict";
+	var Commands = app.getModule("command/Commands");
+	var CommandManager = app.getModule("command/CommandManager");
+	var ExtensionUtils = app.getModule("utils/ExtensionUtils");
+
+	//Buttons creation with jQuery, in the statusbar
+	var $right = $('#statusbar .right-section');
+
+	var leftImgPath = ExtensionUtils.getModulePath(module, "src/left-alignment.png");
+	var rightImgPath = ExtensionUtils.getModulePath(module, "src/right-alignment.png");
+
+	var $closeLeftPannel = $('<div class="close-left item" style="padding:0 1em"><span><img class="k-sprite" src="'+leftImgPath+'" /></span></div>');
+	var $closeRightPannel = $('<div class="close-right item" style="padding:0 1em"><span><img class="k-sprite" src="'+rightImgPath+'" /></span></div>');
+
+	$right.append($closeLeftPannel);
+	$right.append($closeRightPannel);
+
+	//Execute the command "hide" & "show" on the click of the button
+	$('.close-right').click(function() {
+		CommandManager.execute(Commands.VIEW_HIDE_NAVIGATOR);
+	});
+	$('.close-left').click(function() {
+		CommandManager.execute(Commands.VIEW_HIDE_SIDEBAR);
+	});
+	
+
+});
